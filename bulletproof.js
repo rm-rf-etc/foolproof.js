b2332f767d4d643cb154e06a178293fdb302a5a7
Rob Christian <rm.rf.etc@gmail.com>
2013-10-08 23:16:28 -0700

    new functions

--- bulletproof.js
+++ bulletproof.js
@@ -5,18 +5,34 @@
  */
 module.exports = function(){
 
+  /**
+   * @method failWhen
+   * @param {Boolean} condition
+   * @param {Array} args
+   * @param {String} msg
+   * @param {String} level
+   */
+   // @throws {Error}
+  this.failWhen = function failWhen (condition, args, msg, level) {
+    if (isTruthy(condition)) {
+      var msg = (isString(msg)) ? msg : 'Helper received invalid arguments.'
+      var level = (isString(level)) ? level : 'warn'
+      // log(msg, args, level)
+      throw new Error(msg, args)
+    }
+  }
 
   /**
    * @method inArray
    * @param {String}
    * @return {Boolean}
    */
-	this.inArray = function inArray (needle, haystack) {
+  this.inArray = function inArray (needle, haystack) {
     for (var key in haystack)
       if (haystack[key] === needle) return true
 
-	  return false
-	}
+    return false
+  }
 
   /**
    * @method typeOf
@@ -177,6 +193,112 @@ module.exports = function(){
    */
   this.notNull = function notNull (thing) {               return !isNull (thing)         }
 
+
+  /**
+   * @method standardizePath
+   * @param {any object} thing path
+   * @return {Boolean}
+   */
+  this.standardizePath = function validPath (path) {
+    if (path.length > 1) {
+      path = path.replace(/\/{2,}/g, '/')
+      path = path.replace(/^\//, '') // Remove potential leading slash.
+      if (/[^\/]$/.test( path )) path += '/'; // Include trailing slash.
+    }
+    else if ( path !== '/' || /\/\//.test(path) )
+      throw new Error('Path specified is invalid', arguments)
+
+    return path
+  }
+
+  /**
+   * @method isValidPath
+   * @param {String} path
+   * @return {Boolean}
+   */
+  this.isValidPath = function isValidPath (path) {
+    failWhen( isFalsey(path), path )
+    return ( /[A-Za-z0-9._~:\-\/?#@!$&'\[\]()*+,;=%]+/g.test(path) )// && /\/\/{0,1}/g.test(path) )
+  }
+
+  /**
+   * @method saveAsLocal
+   * @param {Array or String} args
+   * @param {Array} params
+   */
+  this.saveAsLocal = function saveAsLocal (args, params) {
+    failWhen( !isString(args) && !isObject(args) && !isArray(params), type, arguments )
+
+    if ( isString(args) && isArray(params) )
+      if (inArray(args, params))
+        eval(arg+' = '+params)
+
+    else if ( isObject(args) && isArray(params) )
+      for (var arg in args)
+        if (inArray(arg, params))
+          eval(arg+' = args[arg]')
+  }
+
+  /**
+   * @method urlParser
+   * @param {Regex} regex
+   * @param {String} url
+   * @param {Array} vars
+   */
+  this.urlParser = function urlParser (regex, url, vars) {
+    var args
+    args = regex.exec(url)
+    args = (args) ? args.slice(1) : []
+
+    if ( isArray(vars) ) {
+      var obj = {}
+      
+      if (vars.length === args.length) {
+        for (var i in vars)
+          obj[vars[i]] = args[i]
+
+        return obj
+      }
+    }
+
+    return args
+  }
+
+
+  var fs = require('fs')
+  var pth = require('path')
+  // Prettier function names.
+  /**
+   * @method isFolder
+   * @param {String} path
+   * @return {Boolean}
+   */
+  this.isFolder  = function isFolder(path){        return fs.lstatSync( path ).isDirectory()           }
+  /**
+   * @method isFile
+   * @param {String} path
+   * @return {Boolean}
+   */
+  this.isFile    = function isFile(path){          return fs.lstatSync( path ).isFile()                }
+  /**
+   * @method filesHere
+   * @param {String} path
+   * @return {Array}
+   */
+  this.filesHere = function filesHere(path){       return fs.readdirSync( path )                       }
+  /**
+   * @method formPath
+   * @param {String} path
+   * @param {String} file
+   * @return {String}
+   */
+  this.formPath  = function formPath(path, file){  return pth.join( path, file )                       }
+  /**
+   * @method fileName
+   * @param {String} path
+   * @return {String}
+   */
+  this.fileName  = function fileName(path){        return pth.basename(path, type.replace(/^\b/,'.'))  }
 
   return this
 }
