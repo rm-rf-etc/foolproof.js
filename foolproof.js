70234e7f2cdd27dd36af7869de504867eabc75e0
Rob Christian <rm.rf.etc@gmail.com>
2013-11-16 20:18:21 -0800

    Add objExtend() and isBoolean() and notBoolean().

--- foolproof.js
+++ foolproof.js
@@ -18,19 +18,17 @@ module.exports = function(){
     this.fSystemLogger = (function(){
 
         /*`'~.,.~'`*\
-             private
+           private
         \*,.~'`'~.,*/
-
         var func = CaughtOnCamera
 
-        function CaughtOnCamera (err)
-        {
+        function CaughtOnCamera (err) {
             console.log('Level: '+err.lvl, err, err.stack)
             throw err
         }
 
         /*`'~.,.~'`*\
-             public
+            public
         \*,.~'`'~.,*/
 
         /**
@@ -42,15 +40,14 @@ module.exports = function(){
          * @param typ {Error} optional, include if you don't want a ReferenceError.
          * @param req {Error} optional, for future integration with node server.
          */
-        function EpicFail (msg, lvl, typ, req)
-        {
+        function EpicFail (msg, lvl, typ, req) {
             var msg = msg || 'No error message was defined for this condition.'
 
             var err
             if ( E(typeof typ) )
-                err = new typ(msg)
+            err = new typ(msg)
             else
-                err = new ReferenceError(msg)
+            err = new ReferenceError(msg)
 
             err.lvl = lvl || 1
 
@@ -62,8 +59,7 @@ module.exports = function(){
          * @for fSystemLogger
          * @param alternate {Function} Set a custom error handler. Will receive an Error object (pass Null to reset).
          */
-        function Use (alternate)
-        {
+        function Use (alternate) {
             if ( isFunction(alternate) )
                 func = alternate
             else if ( isFalsey(alternate) )
@@ -191,8 +187,8 @@ module.exports = function(){
         if (isArray(existing)) {
             additional = [].concat(additional)
             additional.map(function(each){
-                if (!inArray(each,existing))
-                    existing = existing.concat(each)
+            if (!inArray(each,existing))
+                existing = existing.concat(each)
             })
         }
         return existing
@@ -219,8 +215,8 @@ module.exports = function(){
             needles = [].concat(needles)
             
             needles.map(function(each){
-                if (!inArray(each,haystack))
-                    result = result.concat(each)
+            if (!inArray(each,haystack))
+                result = result.concat(each)
             })
             return (result.length < 1) ? null : result
         }
@@ -258,18 +254,18 @@ module.exports = function(){
      */
     this.nameOf = function nameOf (thing)
     {
-        if ( isFunction(thing) ) {
-            if ( /^function\s\(/g.test(thing.toString()) )
-                return 'Anonymous Function'
-            else
-                return /^function\s(\w+?)\(\).*/g.exec( thing.toString() )[1]
-        }
-
-        if (thing !== null && thing !== undefined )
-            return thing.constructor.name
-        
+    if ( isFunction(thing) ) {
+        if ( /^function\s\(/g.test(thing.toString()) )
+            return 'Anonymous Function'
         else
-            return (thing === null) ? 'Null' : 'Undefined'
+            return /^function\s(\w+?)\(\).*/g.exec( thing.toString() )[1]
+    }
+
+    if (thing !== null && thing !== undefined )
+        return thing.constructor.name
+    
+    else
+        return (thing === null) ? 'Null' : 'Undefined'
     }
 
     /**
@@ -288,6 +284,7 @@ module.exports = function(){
                 else return 'number'
             case 'function': return 'function'
             case 'string': return 'string'
+            case 'boolean': return 'boolean'
             default:
                 var type = Object.prototype.toString.apply(thing).toLowerCase().replace(/(^\[object\s|\]$)/g, '')
                 if (type == 'regexp') return 'regex'
@@ -325,6 +322,14 @@ module.exports = function(){
      * @return {Boolean}
      */
     this.isObject = function isObject (thing) {         return (typeOf(thing) === 'object')     }
+
+    /**
+     * @method isBoolean
+     * @for Exports
+     * @param thing {any object}
+     * @return {Boolean}
+     */
+    this.isBoolean = function isBoolean (thing) {       return (typeOf(thing) === 'boolean')     }
 
     /**
      * @method isString
@@ -417,6 +422,14 @@ module.exports = function(){
      * @return {Boolean}
      */
     this.notObject = function notObject (thing) {           return !isObject (thing)       }
+
+    /**
+     * @method notBoolean
+     * @for Exports
+     * @param thing {any object}
+     * @return {Boolean}
+     */
+    this.notBoolean = function notBoolean (thing) {         return !isBoolean (thing)      }
 
     /**
      * @method notString
@@ -521,10 +534,10 @@ module.exports = function(){
             var obj = {}
             
             if (vars.length === args.length) {
-                for (var i in vars)
-                    obj[vars[i]] = args[i]
+            for (var i in vars)
+                obj[vars[i]] = args[i]
 
-                return obj
+            return obj
             }
         }
 
@@ -623,15 +636,86 @@ module.exports = function(){
             var before = found
 
             for (var e in found) {
-                if (e === props[i])
-                    var found = found[props[i]]
+            if (e === props[i])
+                var found = found[props[i]]
             }
 
             if (before === found)
-                return false
+            return false
         }
 
-        return found
+    return found
+    }
+
+    /**
+     * @method objExtend
+     * @for Exports
+     * @param obj1 {Object}
+     * @param obj2 {Object}
+     */
+    this.objExtend = function objExtend(obj1, obj2)
+    {
+        var options, name, src, copy, copyIsArray, clone, target, length, deep
+
+        target = arguments[0] || {}
+        i = 1
+        length = arguments.length
+        deep = false
+
+        // Handle a deep copy situation
+        if ( isBoolean(target) ) {
+            deep = target
+
+            // skip the boolean and the target
+            target = arguments[ i ] || {}
+            i++
+        }
+
+        // Handle case when target is a string or something (possible in deep copy)
+        if ( typeof target !== "object" && !jQuery.isFunction(target) ) {
+            target = {}
+        }
+
+        // extend jQuery itself if only one argument is passed
+        if ( i === length ) {
+            target = this
+            i--
+        }
+
+        for ( ; i < length; i++ ) {
+            // Only deal with non-null/undefined values
+            if ( (options = arguments[ i ]) != null ) {
+                // Extend the base object
+                for ( name in options ) {
+                    src = target[ name ]
+                    copy = options[ name ]
+
+                    // Prevent never-ending loop
+                    if ( target === copy ) {
+                        continue
+                    }
+
+                    // Recurse if we're merging plain objects or arrays
+                    if ( deep && copy && ( isObject(copy) || (copyIsArray = isArray(copy)) ) ) {
+                        if ( copyIsArray ) {
+                            copyIsArray = false
+                            clone = src && isArray(src) ? src : []
+                        }
+                        else
+                            clone = src && isObject(src) ? src : {}
+
+                        // Never move original objects, clone them
+                        target[ name ] = extend( deep, clone, copy )
+
+                    // Don't bring in undefined values
+                    }
+                    else if ( copy !== undefined )
+                        target[ name ] = copy
+                }
+            }
+        }
+        // Return the modified object
+        return target
     }
 
 
